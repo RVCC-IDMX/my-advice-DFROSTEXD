18c1461ec5c77e3dc135367eb4cdf5df725e632d
DFROSTEXD <willh6789@gmail.com>
2026-04-02 11:45:37 -0400

    Add experiments.js for DOM manipulation practice and learning

---
+++ src/js/experiments.js
@@ -0,0 +1,14 @@
+/**
+ * DOM Experiments - Learning by doing
+ * This file is a sandbox for trying out DOM manipulation methods
+ */
+
+// Experiment 1: Change text on the page
+
+// Experiment 2: Toggle a class
+
+// Experiment 3: Count elements
+
+// Experiment 4: Add something new to the page
+
+// Experiment 5: Modify multiple elements
